26b4016d2d063d800b9db6f18e5a05be9762e387
Halo104 <christopheryim@hotmail.com>
2021-11-27 15:31:30 +0000

    added tests.py, changed image_augmentations. for tests.py, two tests were created for testing the new greyscale function in image_augmentations, one to test that the input and output image fromthe function is the same size and second is to test that all pixels are actually greyscale

--- app/image_augmentations.py
+++ app/image_augmentations.py
@@ -2,24 +2,29 @@ from PIL import Image
 import random
 
 
-def random_augmentation(image: Image):
-    augmentations = [rotate, fliplr, do_nothing, zoom]
+def random_augmentation(image: Image) -> Image:
+    #augmentations = [rotate, fliplr, do_nothing, zoom, greyscale]
+    augmentations = [greyscale]
     func = random.choice(augmentations)
     return func(image)
 
 
-def do_nothing(image: Image):
+def do_nothing(image: Image) -> Image:
     return image
 
 
-def fliplr(image: Image):
+def fliplr(image: Image) -> Image:
     return image.transpose(Image.FLIP_LEFT_RIGHT)
 
 
-def rotate(image: Image):
+def rotate(image: Image) -> Image:
     return image.rotate(random.randint(0, 365))
 
 
-def zoom(image: Image):
+def zoom(image: Image) -> Image:
     zoomRatio = 1.5
     return image.transform(image.size, Image.EXTENT, data=(0, 1, image.width // zoomRatio, image.height // zoomRatio))
+
+
+def greyscale(image: Image) -> Image:
+    return image.convert('L')
---
+++ app/tests.py
@@ -0,0 +1,35 @@
+import unittest
+
+from PIL import Image
+
+from image_augmentations import greyscale
+
+
+def create_test_image():
+    test_image = Image.new('RGB', size=(100, 200), color=(200, 50, 50))
+    return test_image
+
+
+class ImageAugmentationsTest(unittest.TestCase):
+    def test_greyscale_image_is_same_size(self):
+        test_image = create_test_image()
+        self.assertEqual(greyscale(test_image).width, 100, "Width should be 100")
+        self.assertEqual(greyscale(test_image).height, 200, "Height should be 200")
+
+    def test_greyscale_image_is_grey(self):
+        test_image = greyscale(create_test_image())
+        test_image = test_image.convert('RGB')
+        width, height = test_image.size
+        for w in range(width):
+            for h in range(height):
+                r, g, b = test_image.getpixel((w, h))
+                self.assertEqual(r, g, "red and green should be same value")
+                self.assertEqual(r, b, "red and blue should be same value")
+
+        #Another method of testing for greyscale, by assuming only one colour band exsists
+        #self.assertEqual(len(test_image.getbands()), 1, "Should have only 1 band")
+
+
+if __name__ == '__main__':
+    # Run all the tests
+    unittest.main()
